8fb846ec7e45974c01086721a9e1ee90327d9d92
hamadakafu <kafu.h1998@gmail.com>
2021-08-01 22:58:36 +0900

    syoujin

---
+++ src/bin/typical90_20.rs
@@ -0,0 +1,25 @@
+use std::cmp::{max, min};
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+use whiteread::parse_line;
+
+const ten97: usize = 1000000007;
+fn alphabet2idx(c: char) -> usize {
+    if c.is_ascii_lowercase() {
+        c as u8 as usize - 'a' as u8 as usize
+    } else if c.is_ascii_uppercase() {
+        c as u8 as usize - 'A' as u8 as usize
+    } else {
+        panic!("wtf")
+    }
+}
+
+fn main() {
+    let (a, b, c): (usize, usize, usize) = parse_line().unwrap();
+    if a < c.pow(b as u32) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
